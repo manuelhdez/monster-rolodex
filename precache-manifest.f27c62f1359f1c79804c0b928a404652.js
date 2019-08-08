4d1b8ac79650dbae62865c2658adb5cd0df32b85
Manuel Hernandez <ing.merhandez@gmail.com>
2019-08-07 22:49:08 -0600

    Updates

---
+++ precache-manifest.f27c62f1359f1c79804c0b928a404652.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bcf79a3466976c26cf059cce69fb2ed",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "3c4be062e047e5a2ade1",
+    "url": "/monster-rolodex/static/css/main.c51844c1.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "3c4be062e047e5a2ade1",
+    "url": "/monster-rolodex/static/js/main.1e240ea4.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);

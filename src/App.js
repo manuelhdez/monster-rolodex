b68fb3094100ec2c51707a3f9c843b594dd2cfc2
Manuel Hernandez <ing.merhandez@gmail.com>
2019-08-07 23:11:33 -0600

    adding files for github pages 3

--- src/App.js
+++ src/App.js
@@ -17,9 +17,7 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    fetch(
-      "https://cors-anywhere.herokuapp.com/https://jsonplaceholder.typicode.com/users"
-    )
+    fetch("https://jsonplaceholder.typicode.com/users", { mode: "no-cors" })
       .then(response => response.json())
       .then(users => {
         console.log(users);
